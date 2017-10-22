916f023baebfcef463b02d874a0be3b0f6244ff9
kalbter <shilec_alexey_1996_sosh59@mail.ru>
2017-10-22 20:51:02 +0300

    from work

---
+++ Find_Files/FindFilesWithIEnumerabe.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Find_files
+{
+    class FindFilesWithIEnumerabe
+    {
+        public List<string> fileList = new List<string>();
+        private IEnumerable<string> FindVoid(string rootDirectory)
+        {
+            var content = Directory.EnumerateFileSystemEntries(rootDirectory).ToList();
+            foreach (var folderItem in content)
+            {
+                FileAttributes attr = File.GetAttributes(folderItem);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    var htmlFiles = Directory.GetFiles(folderItem, "*.html");
+                    foreach (var htmlItem in htmlFiles)
+                        yield return htmlItem;
+                    foreach (var subFolderItem in FindVoid(folderItem))
+                        yield return subFolderItem;
+                }
+            }
+        }
+        public void FindFiles()
+        {
+        Get_Path:
+            Console.WriteLine("Введите путь каталога:");
+            string path = Console.ReadLine();
+            if (Directory.Exists(path))
+            {
+                fileList.AddRange(Directory.GetFiles(path).Where(f => f.EndsWith(".html")));
+                fileList.AddRange(FindVoid(path));
+                foreach (var item in fileList)
+                {
+                    Console.WriteLine(item);
+                }
+                if (fileList.Count == 0)
+                {
+                    Console.WriteLine("Нет файлов");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибочка");
+                goto Get_Path;
+            }
+        }
+    }
+
+}
---
+++ Find_Files/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+
+namespace Find_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Get_path:
+            Console.WriteLine("Вариант[1/2]");
+            var a = Console.ReadLine();
+            switch (a)
+            {
+                case "1":
+                    Console.WriteLine("Вы выбрали вариант1");
+                    break;
+                case "2":
+                    Console.WriteLine("Вы выбрали вариант2");
+                    break;
+                default:
+                    Console.WriteLine("1 или 2");
+                    goto Get_path;
+            }
+            if (a == "1")
+            {
+                FindFilesWithList file = new FindFilesWithList();
+                try
+                {
+                    file.FindFiles();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    goto Get_path;
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                FindFilesWithIEnumerabe file = new FindFilesWithIEnumerabe();
+                try
+                {
+                    file.FindFiles();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    goto Get_path;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
